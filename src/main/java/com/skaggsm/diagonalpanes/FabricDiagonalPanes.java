5b7dc9847aa31af40ad2430040de6157431fd8ec
Mitchell Skaggs <skaggsm333@gmail.com>
2019-08-31 20:02:35 -0500

    Initial commit

---
+++ src/main/java/com/skaggsm/diagonalpanes/FabricDiagonalPanes.java
@@ -0,0 +1,11 @@
+package com.skaggsm.diagonalpanes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricDiagonalPanes implements ModInitializer {
+    public static final String MODID = "fabric-diagonal-panes";
+
+    @Override
+    public void onInitialize() {
+    }
+}
