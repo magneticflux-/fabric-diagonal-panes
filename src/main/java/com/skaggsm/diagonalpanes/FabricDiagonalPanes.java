c7c29b2d1b921b06738d4a92a9985ea67c83a220
Mitchell Skaggs <skaggsm333@gmail.com>
2020-04-23 02:26:20 -0500

    Add Cotton dependency and virtual resourcepack

--- src/main/java/com/skaggsm/diagonalpanes/FabricDiagonalPanes.java
+++ src/main/java/com/skaggsm/diagonalpanes/FabricDiagonalPanes.java
@@ -1,11 +1,20 @@
 package com.skaggsm.diagonalpanes;
 
+import io.github.cottonmc.cotton.datapack.virtual.VirtualResourcePack;
+import io.github.cottonmc.cotton.datapack.virtual.VirtualResourcePackManager;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.util.Identifier;
+
+import java.util.HashMap;
+
+import static java.util.Collections.singletonList;
 
 public class FabricDiagonalPanes implements ModInitializer {
     public static final String MODID = "fabric-diagonal-panes";
 
     @Override
     public void onInitialize() {
+        VirtualResourcePackManager.INSTANCE.addPack(new VirtualResourcePack(new Identifier("test", "test"), new HashMap<>()), singletonList(ResourceType.CLIENT_RESOURCES));
     }
 }
