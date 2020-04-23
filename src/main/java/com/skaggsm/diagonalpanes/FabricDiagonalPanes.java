21a86a7ef939f965b43fd01f26d16702a75da520
Mitchell Skaggs <skaggsm333@gmail.com>
2020-04-23 18:18:54 -0500

    Load diagonal models in a virtual resource pack

--- src/main/java/com/skaggsm/diagonalpanes/FabricDiagonalPanes.java
+++ src/main/java/com/skaggsm/diagonalpanes/FabricDiagonalPanes.java
@@ -1,5 +1,6 @@
 package com.skaggsm.diagonalpanes;
 
+import io.github.cottonmc.cotton.datapack.virtual.InputStreamProvider;
 import io.github.cottonmc.cotton.datapack.virtual.VirtualResourcePack;
 import io.github.cottonmc.cotton.datapack.virtual.VirtualResourcePackManager;
 import net.fabricmc.api.ModInitializer;
@@ -11,10 +12,52 @@ import java.util.HashMap;
 import static java.util.Collections.singletonList;
 
 public class FabricDiagonalPanes implements ModInitializer {
+
     public static final String MODID = "fabric-diagonal-panes";
+
+    private static final String[] COLORS = new String[]{
+            "black",
+            "blue",
+            "brown",
+            "cyan",
+            "gray",
+            "green",
+            "light_blue",
+            "light_gray",
+            "lime",
+            "magenta",
+            "orange",
+            "pink",
+            "purple",
+            "red",
+            "white",
+            "yellow"
+    };
+
+    private static void registerPath(String path, HashMap<String, InputStreamProvider> map) {
+        map.put(path, () -> FabricDiagonalPanes.class.getResourceAsStream(String.format("/virtual-%s", path)));
+    }
 
     @Override
     public void onInitialize() {
-        VirtualResourcePackManager.INSTANCE.addPack(new VirtualResourcePack(new Identifier("test", "test"), new HashMap<>()), singletonList(ResourceType.CLIENT_RESOURCES));
+        HashMap<String, InputStreamProvider> map = new HashMap<>();
+
+        registerPath("assets/minecraft/models/block/template_glass_pane_diagonal.json", map);
+
+        registerPath("assets/minecraft/models/block/glass_pane_diagonal.json", map);
+        registerPath("assets/minecraft/blockstates/glass_pane.json", map);
+
+        registerPath("assets/minecraft/models/block/iron_bars_diagonal.json", map);
+        registerPath("assets/minecraft/blockstates/iron_bars.json", map);
+
+        for (String color : COLORS) {
+            registerPath(String.format("assets/minecraft/models/block/%s_stained_glass_pane_diagonal.json", color), map);
+            registerPath(String.format("assets/minecraft/blockstates/%s_stained_glass_pane.json", color), map);
+        }
+
+        VirtualResourcePackManager.INSTANCE.addPack(
+                new VirtualResourcePack(new Identifier(MODID, "models"), map),
+                singletonList(ResourceType.CLIENT_RESOURCES)
+        );
     }
 }
