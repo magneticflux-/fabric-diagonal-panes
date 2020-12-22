4911b9d0ddded799c2aa1f6557ed51b498ad73ea
Mitchell Skaggs <skaggsm333@gmail.com>
2020-12-22 12:19:27 -0600

    Migrate from Cotton to official Fabric API and remove a ton of unused code

--- src/main/java/com/skaggsm/diagonalpanes/FabricDiagonalPanes.java
+++ src/main/java/com/skaggsm/diagonalpanes/FabricDiagonalPanes.java
@@ -1,63 +1,28 @@
 package com.skaggsm.diagonalpanes;
 
-import io.github.cottonmc.cotton.datapack.virtual.InputStreamProvider;
-import io.github.cottonmc.cotton.datapack.virtual.VirtualResourcePack;
-import io.github.cottonmc.cotton.datapack.virtual.VirtualResourcePackManager;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.resource.ResourceType;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.ModContainer;
 import net.minecraft.util.Identifier;
-
-import java.util.HashMap;
-
-import static java.util.Collections.singletonList;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class FabricDiagonalPanes implements ModInitializer {
 
     public static final String MODID = "fabric-diagonal-panes";
-
-    private static final String[] COLORS = new String[]{
-            "black",
-            "blue",
-            "brown",
-            "cyan",
-            "gray",
-            "green",
-            "light_blue",
-            "light_gray",
-            "lime",
-            "magenta",
-            "orange",
-            "pink",
-            "purple",
-            "red",
-            "white",
-            "yellow"
-    };
-
-    private static void registerPath(String path, HashMap<String, InputStreamProvider> map) {
-        map.put(path, () -> FabricDiagonalPanes.class.getResourceAsStream(String.format("/virtual-%s", path)));
-    }
+    private static final Logger LOGGER = LogManager.getLogger();
 
     @Override
     public void onInitialize() {
-        HashMap<String, InputStreamProvider> map = new HashMap<>();
-
-        registerPath("assets/minecraft/models/block/template_glass_pane_diagonal.json", map);
-
-        registerPath("assets/minecraft/models/block/glass_pane_diagonal.json", map);
-        registerPath("assets/minecraft/blockstates/glass_pane.json", map);
-
-        registerPath("assets/minecraft/models/block/iron_bars_diagonal.json", map);
-        registerPath("assets/minecraft/blockstates/iron_bars.json", map);
-
-        for (String color : COLORS) {
-            registerPath(String.format("assets/minecraft/models/block/%s_stained_glass_pane_diagonal.json", color), map);
-            registerPath(String.format("assets/minecraft/blockstates/%s_stained_glass_pane.json", color), map);
-        }
-
-        VirtualResourcePackManager.INSTANCE.addPack(
-                new VirtualResourcePack(new Identifier(MODID, "models"), map),
-                singletonList(ResourceType.CLIENT_RESOURCES)
+        ModContainer modContainer = FabricLoader.getInstance().getModContainer(MODID).orElseThrow(RuntimeException::new);
+        boolean success = ResourceManagerHelper.registerBuiltinResourcePack(
+                new Identifier(MODID, "default"),
+                "resourcepack",
+                modContainer,
+                true
         );
+        if (!success)
+            LOGGER.warn("Failed to register built-in resource pack!");
     }
 }
