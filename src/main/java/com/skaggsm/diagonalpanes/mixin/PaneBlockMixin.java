d574a95ab3e5e1235cdc385e8663b501750c8fb5
Mitchell Skaggs <skaggsm333@gmail.com>
2020-12-24 14:15:55 -0600

    Add custom outline by tricking the renderer with a phony VoxelShape

--- src/main/java/com/skaggsm/diagonalpanes/mixin/PaneBlockMixin.java
+++ src/main/java/com/skaggsm/diagonalpanes/mixin/PaneBlockMixin.java
@@ -1,9 +1,11 @@
 package com.skaggsm.diagonalpanes.mixin;
 
+import com.skaggsm.diagonalpanes.NotVoxelShape;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.HorizontalConnectingBlock;
 import net.minecraft.block.PaneBlock;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import org.spongepowered.asm.mixin.Mixin;
@@ -12,6 +14,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mixin(PaneBlock.class)
 public abstract class PaneBlockMixin extends HorizontalConnectingBlock implements HorizontalConnectingBlockAccessor {
@@ -40,9 +44,9 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 Block.createCuboidShape(7, 0, 15, 8, 16, 16)
         );
         VoxelShape westSouthBounds = VoxelShapes.union(
-                Block.createCuboidShape(0, 0, 8, 3, 16, 11),
-                Block.createCuboidShape(3, 0, 11, 5, 16, 13),
-                Block.createCuboidShape(5, 0, 13, 8, 16, 16)
+                Block.createCuboidShape(0, 0, 8, 4, 16, 12),
+                Block.createCuboidShape(2, 0, 10, 6, 16, 14),
+                Block.createCuboidShape(4, 0, 12, 8, 16, 16)
         );
 
         VoxelShape northWestCollision = VoxelShapes.union(
@@ -56,9 +60,9 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 Block.createCuboidShape(7, 0, 0, 8, 16, 1)
         );
         VoxelShape northWestBounds = VoxelShapes.union(
-                Block.createCuboidShape(0, 0, 5, 3, 16, 8),
-                Block.createCuboidShape(3, 0, 3, 5, 16, 5),
-                Block.createCuboidShape(5, 0, 0, 8, 16, 3)
+                Block.createCuboidShape(0, 0, 4, 4, 16, 8),
+                Block.createCuboidShape(2, 0, 2, 6, 16, 6),
+                Block.createCuboidShape(4, 0, 0, 8, 16, 4)
         );
 
         VoxelShape eastSouthCollision = VoxelShapes.union(
@@ -72,9 +76,9 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 Block.createCuboidShape(8, 0, 15, 9, 16, 16)
         );
         VoxelShape eastSouthBounds = VoxelShapes.union(
-                Block.createCuboidShape(13, 0, 8, 16, 16, 11),
-                Block.createCuboidShape(11, 0, 11, 13, 16, 13),
-                Block.createCuboidShape(8, 0, 13, 11, 16, 16)
+                Block.createCuboidShape(12, 0, 8, 16, 16, 12),
+                Block.createCuboidShape(10, 0, 10, 14, 16, 14),
+                Block.createCuboidShape(8, 0, 12, 12, 16, 16)
         );
 
         VoxelShape eastNorthCollision = VoxelShapes.union(
@@ -88,9 +92,9 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 Block.createCuboidShape(8, 0, 0, 9, 16, 1)
         );
         VoxelShape eastNorthBounds = VoxelShapes.union(
-                Block.createCuboidShape(13, 0, 5, 16, 16, 8),
-                Block.createCuboidShape(11, 0, 3, 13, 16, 5),
-                Block.createCuboidShape(8, 0, 0, 11, 16, 3)
+                Block.createCuboidShape(12, 0, 4, 16, 16, 8),
+                Block.createCuboidShape(10, 0, 2, 14, 16, 6),
+                Block.createCuboidShape(8, 0, 0, 12, 16, 4)
         );
 
         /*
@@ -141,12 +145,58 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
         };
 
         VoxelShape[] boundingShapes = Arrays.copyOf(collisionShapes, collisionShapes.length);
-        boundingShapes[3] = westSouthBounds;
-        boundingShapes[6] = northWestBounds;
-        boundingShapes[9] = eastSouthBounds;
-        boundingShapes[12] = eastNorthBounds;
+        // 0.9558058261758 0.0441941738242
+        // 0.5441941738242 0.4558058261758
+        Vec3d p1 = new Vec3d(-0.0441941738242, 0, 0.5441941738242);
+        Vec3d p2 = new Vec3d(0.4558058261758, 0, 1.0441941738242);
+        Vec3d p3 = new Vec3d(0.5441941738242, 0, 0.9558058261758);
+        Vec3d p4 = new Vec3d(0.0441941738242, 0, 0.4558058261758);
+        Vec3d p1u16 = p1.add(0, 1, 0);
+        Vec3d p2u16 = p2.add(0, 1, 0);
+        Vec3d p3u16 = p3.add(0, 1, 0);
+        Vec3d p4u16 = p4.add(0, 1, 0);
+
+        List<Vec3d> westSouthBoundingEdges = Arrays.asList(
+                // Bottom
+                p1, p2,
+                p2, p3,
+                p3, p4,
+                p4, p1,
+
+                // Sides
+                p1, p1u16,
+                p2, p2u16,
+                p3, p3u16,
+                p4, p4u16,
+
+                // Top
+                p1u16, p2u16,
+                p2u16, p3u16,
+                p3u16, p4u16,
+                p4u16, p1u16
+        );
+        List<Vec3d> northWestBoundingEdges = westSouthBoundingEdges.stream().map(PaneBlockMixin::flipZ).collect(Collectors.toList());
+        List<Vec3d> eastSouthBoundingEdges = westSouthBoundingEdges.stream().map(PaneBlockMixin::flipX).collect(Collectors.toList());
+        List<Vec3d> eastNorthBoundingEdges = westSouthBoundingEdges.stream().map(PaneBlockMixin::flipBoth).collect(Collectors.toList());
+
+        boundingShapes[3] = new NotVoxelShape(westSouthBounds, westSouthBoundingEdges);
+        boundingShapes[6] = new NotVoxelShape(northWestBounds, northWestBoundingEdges);
+        boundingShapes[9] = new NotVoxelShape(eastSouthBounds, eastSouthBoundingEdges);
+        boundingShapes[12] = new NotVoxelShape(eastNorthBounds, eastNorthBoundingEdges);
 
         this.setCollisionShapes(collisionShapes);
         this.setBoundingShapes(boundingShapes);
+    }
+
+    private static Vec3d flipX(Vec3d in) {
+        return new Vec3d(1 - in.x, in.y, in.z);
+    }
+
+    private static Vec3d flipZ(Vec3d in) {
+        return new Vec3d(in.x, in.y, 1 - in.z);
+    }
+
+    private static Vec3d flipBoth(Vec3d in) {
+        return flipX(flipZ(in));
     }
 }
