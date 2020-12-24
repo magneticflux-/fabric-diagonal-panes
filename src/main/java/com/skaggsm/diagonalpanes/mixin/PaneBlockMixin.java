74f727740d8773cb6a8c8d02890f40c0ac9cf651
Mitchell Skaggs <skaggsm333@gmail.com>
2020-12-24 14:30:18 -0600

    Tweak raycasting bounding box to improve selection precision

--- src/main/java/com/skaggsm/diagonalpanes/mixin/PaneBlockMixin.java
+++ src/main/java/com/skaggsm/diagonalpanes/mixin/PaneBlockMixin.java
@@ -44,9 +44,13 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 Block.createCuboidShape(7, 0, 15, 8, 16, 16)
         );
         VoxelShape westSouthBounds = VoxelShapes.union(
-                Block.createCuboidShape(0, 0, 8, 4, 16, 12),
-                Block.createCuboidShape(2, 0, 10, 6, 16, 14),
-                Block.createCuboidShape(4, 0, 12, 8, 16, 16)
+                Block.createCuboidShape(0, 0, 8, 2, 16, 10),
+                Block.createCuboidShape(1, 0, 9, 3, 16, 11),
+                Block.createCuboidShape(2, 0, 10, 4, 16, 12),
+                Block.createCuboidShape(3, 0, 11, 5, 16, 13),
+                Block.createCuboidShape(4, 0, 12, 6, 16, 14),
+                Block.createCuboidShape(5, 0, 13, 7, 16, 15),
+                Block.createCuboidShape(6, 0, 14, 8, 16, 16)
         );
 
         VoxelShape northWestCollision = VoxelShapes.union(
@@ -60,9 +64,13 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 Block.createCuboidShape(7, 0, 0, 8, 16, 1)
         );
         VoxelShape northWestBounds = VoxelShapes.union(
-                Block.createCuboidShape(0, 0, 4, 4, 16, 8),
-                Block.createCuboidShape(2, 0, 2, 6, 16, 6),
-                Block.createCuboidShape(4, 0, 0, 8, 16, 4)
+                Block.createCuboidShape(0, 0, 6, 2, 16, 8),
+                Block.createCuboidShape(1, 0, 5, 3, 16, 7),
+                Block.createCuboidShape(2, 0, 4, 4, 16, 6),
+                Block.createCuboidShape(3, 0, 3, 5, 16, 5),
+                Block.createCuboidShape(4, 0, 2, 6, 16, 4),
+                Block.createCuboidShape(5, 0, 1, 7, 16, 3),
+                Block.createCuboidShape(6, 0, 0, 8, 16, 2)
         );
 
         VoxelShape eastSouthCollision = VoxelShapes.union(
@@ -76,9 +84,13 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 Block.createCuboidShape(8, 0, 15, 9, 16, 16)
         );
         VoxelShape eastSouthBounds = VoxelShapes.union(
-                Block.createCuboidShape(12, 0, 8, 16, 16, 12),
-                Block.createCuboidShape(10, 0, 10, 14, 16, 14),
-                Block.createCuboidShape(8, 0, 12, 12, 16, 16)
+                Block.createCuboidShape(14, 0, 8, 16, 16, 10),
+                Block.createCuboidShape(13, 0, 9, 15, 16, 11),
+                Block.createCuboidShape(12, 0, 10, 14, 16, 12),
+                Block.createCuboidShape(11, 0, 11, 13, 16, 13),
+                Block.createCuboidShape(10, 0, 12, 12, 16, 14),
+                Block.createCuboidShape(9, 0, 13, 11, 16, 15),
+                Block.createCuboidShape(8, 0, 14, 10, 16, 16)
         );
 
         VoxelShape eastNorthCollision = VoxelShapes.union(
@@ -92,9 +104,13 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 Block.createCuboidShape(8, 0, 0, 9, 16, 1)
         );
         VoxelShape eastNorthBounds = VoxelShapes.union(
-                Block.createCuboidShape(12, 0, 4, 16, 16, 8),
-                Block.createCuboidShape(10, 0, 2, 14, 16, 6),
-                Block.createCuboidShape(8, 0, 0, 12, 16, 4)
+                Block.createCuboidShape(14, 0, 6, 16, 16, 8),
+                Block.createCuboidShape(13, 0, 5, 15, 16, 7),
+                Block.createCuboidShape(12, 0, 4, 14, 16, 6),
+                Block.createCuboidShape(11, 0, 3, 13, 16, 5),
+                Block.createCuboidShape(10, 0, 2, 12, 16, 4),
+                Block.createCuboidShape(9, 0, 1, 11, 16, 3),
+                Block.createCuboidShape(8, 0, 0, 10, 16, 2)
         );
 
         /*
@@ -143,6 +159,8 @@ public abstract class PaneBlockMixin extends HorizontalConnectingBlock implement
                 // 1111
                 VoxelShapes.union(center, east, north, west, south),
         };
+
+        // Non-voxel bounding box setup:
 
         VoxelShape[] boundingShapes = Arrays.copyOf(collisionShapes, collisionShapes.length);
         // 0.9558058261758 0.0441941738242
